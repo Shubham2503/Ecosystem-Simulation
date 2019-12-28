ef2e1684d34ab59a84f13f94e4a3f9b21596cd9b
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2019-12-28 12:13:22 +0530

    Removed unnecessary assets

---
+++ Ecosystem Simulation/Assets/Script/control_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control_script : MonoBehaviour
+{
+
+    public AnimationControl anm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anm.SetAnimation("isWalking");
+            
+        }
+        
+    }
+}
