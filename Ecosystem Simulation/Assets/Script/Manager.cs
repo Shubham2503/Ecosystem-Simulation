fe57b12f786d6a1ece590a23e83f97dc14701873
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-01-16 22:39:13 +0530

    dynamical change in display according to input key

--- Ecosystem Simulation/Assets/Script/Manager.cs
+++ Ecosystem Simulation/Assets/Script/Manager.cs
@@ -10,21 +10,28 @@ public class Manager : MonoBehaviour
     public int animal;
     public int food;
     public int numberOfNodeInGraph = 20;
-
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
     private int k;
-    public List<int> list;
-    public List<int> list2;
+    private List<int> list;
+    private List<int> list2;
 
 
+    AudioListener cameraOneAudioLis;
+    AudioListener cameraTwoAudioLis;
     public Window_Graph g;
     public Window_Graph g2;
 
     void Start()
     {
+        cameraOneAudioLis = cameraOne.GetComponent<AudioListener>();
+        cameraTwoAudioLis = cameraTwo.GetComponent<AudioListener>();
         animal = PlayerPrefs.GetInt("animal", 0);
         food = PlayerPrefs.GetInt("food", 0);
         list = new List<int>();
         list2 = new List<int>();
+
+        cameraPositionChange(PlayerPrefs.GetInt("CameraPosition"));
     }
 
     void Update()
@@ -39,6 +46,56 @@ public class Manager : MonoBehaviour
             g.ShowGraph(list, numberOfNodeInGraph, (int _i) => "Day " + (_i + 1), (float _f) => "Rabbits " + Mathf.RoundToInt(_f));
             g2.ShowGraph(list2, numberOfNodeInGraph, (int _i) => "Day " + (_i + 1), (float _f) => "food " + Mathf.RoundToInt(_f));
             k = 0;
+        }
+
+        switchCamera();
+    }
+    //Change Camera Keyboard
+    void switchCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt))
+        {
+            cameraChangeCounter();
+        }
+    }
+
+    //Camera Counter
+    void cameraChangeCounter()
+    {
+        int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
+        cameraPositionCounter++;
+        cameraPositionChange(cameraPositionCounter);
+    }
+
+    //Camera change Logic
+    void cameraPositionChange(int camPosition)
+    {
+        if (camPosition > 1)
+        {
+            camPosition = 0;
+        }
+
+        //Set camera position database
+        PlayerPrefs.SetInt("CameraPosition", camPosition);
+
+        //Set camera position 1
+        if (camPosition == 0)
+        {
+            cameraOne.SetActive(true);
+            cameraOneAudioLis.enabled = true;
+
+            cameraTwoAudioLis.enabled = false;
+            cameraTwo.SetActive(false);
+        }
+
+        //Set camera position 2
+        if (camPosition == 1)
+        {
+            cameraTwo.SetActive(true);
+            cameraTwoAudioLis.enabled = true;
+
+            cameraOneAudioLis.enabled = false;
+            cameraOne.SetActive(false);
         }
 
     }
